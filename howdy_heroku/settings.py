418493259f63956b5a6192aa7601496aeb2a51c3
hpaasch <hepaasch@gmail.com>
2016-07-07 11:47:23 -0400

    getting started

--- howdy_heroku/settings.py
+++ howdy_heroku/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taco_app',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'howdy_heroku.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR 
+STATIC_ROOT = BASE_DIR
---
+++ taco_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TacoAppConfig(AppConfig):
+    name = 'taco_app'
